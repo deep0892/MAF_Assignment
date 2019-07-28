3614354c8498f405eb395e8326e44307ad6c0632
Deepankar <deepankar@policybazaar.com>
2019-07-28 11:23:58 +0530

    assignment

--- index.js
+++ index.js
@@ -158,9 +158,11 @@ let Program = function() {
 };
 //============================================================
 let p = new Program();
-p.ProcessInput([
+//Your input goes here
+let input = [
   "1 imported bottle of perfume at 27.99",
   "1 bottle of perfume at 18.99",
   "1 packet of headache pills at 9.75",
   "1 box of imported chocolates at 11.25"
-]);
+];
+p.ProcessInput(input);
